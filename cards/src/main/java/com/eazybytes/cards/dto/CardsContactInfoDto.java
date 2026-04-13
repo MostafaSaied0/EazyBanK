9731873202de37940f4bab55ffb0f13319730612
Saied0 <saidsaidsaidali888@gmail.com>
2026-04-13 11:29:26 +0200

    Enhance

--- cards/src/main/java/com/eazybytes/cards/dto/CardsContactInfoDto.java
+++ cards/src/main/java/com/eazybytes/cards/dto/CardsContactInfoDto.java
@@ -1,10 +1,15 @@
 package com.eazybytes.cards.dto;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
-
+@Data
 @ConfigurationProperties(prefix = "cards")
-public record CardsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+public class CardsContactInfoDto{
+
+    String message;
+    Map<String, String> contactDetails;
+    List<String> onCallSupport;
 }
